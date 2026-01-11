66771d9c4de249d8a87c8658fa47169fb9f60566
Rongjiang Wang <wangatberlin@gmail.com>
2026-01-11 11:11:36 +0100

    Add files via upload

--- SDM2025/SourceCode/sdmgrnmat.f
+++ SDM2025/SourceCode/sdmgrnmat.f
@@ -65,18 +65,18 @@ c
         enddo
       enddo
 c
-c      if(izhy.eq.0)return
-c
-c      do n=1,nps*2
-c        sd=0.d0
-c        do m=1,nps*nsmocmp
-c          sd=sd+smomat(m,n)**2
-c        enddo
-c        sd=dsqrt(sd)
-c        do m=1,nps*nsmocmp
-c          smomat(m,n)=smomat(m,n)/sd
-c        enddo
-c      enddo
+      if(izhy.eq.0)return
+
+      do n=1,nps*2
+        sd=0.d0
+        do m=1,nps*nsmocmp
+          sd=sd+smomat(m,n)**2
+        enddo
+        sd=dsqrt(sd)
+        do m=1,nps*nsmocmp
+          smomat(m,n)=smomat(m,n)/sd
+        enddo
+      enddo
 c
       return
       end
